2b08670e8778fdee253d6631f671c67f5b725357
mpagano07 <matias.pagano07@gmail.com>
2026-05-10 15:45:13 -0300

    fix nextresponse import

--- src/app/auth/logout/route.ts
+++ src/app/auth/logout/route.ts
@@ -1,13 +1,13 @@
 import { createClient } from '@/utils/supabase/server'
 import { revalidatePath } from 'next/cache'
-import { NextResponse } from 'next/response'
+import { NextResponse } from "next/server"
 
 export async function POST(request: Request) {
   const supabase = createClient()
   await supabase.auth.signOut()
 
   revalidatePath('/', 'layout')
-  
+
   return NextResponse.redirect(new URL('/', request.url), {
     status: 302,
   })
